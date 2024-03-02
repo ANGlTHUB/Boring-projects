173c8e8820d822786c9f52b785e13064d4fe3467
Achraf Nouri <achref.nouri.pro@gmail.com>
2024-03-02 05:36:58 +0300

    Create typewriter.js

---
+++ typewriter.js
@@ -0,0 +1,15 @@
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('Hello World!')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Strings can be removed')
+    .pauseFor(2500)
+    .deleteChars(7)
+    .typeString('<strong>altered!</strong>')
+    .pauseFor(2500)
+    .start();
